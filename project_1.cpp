976a30a539eacfd2bd9ad2e0e57afa8a1bbdb9aa
adrnna <71454952+adrnna@users.noreply.github.com>
2022-11-15 13:17:32 +0100

    Update project_1.cpp

--- project_1.cpp
+++ project_1.cpp
@@ -5,14 +5,19 @@
 #include <stdlib.h>
 using namespace std;
 int height = 20, width = 20, i, j, gameOver, score, userInput;
+char h[] = "|";
+char w[] = "_";
 
 
 //Function to draw a boundary
 void draw() {
 	for (i = 0; i < height; ++i) {
 		for (j = 0; j < width; j++) {
-			if (i == 0 || i == width - 1 || j == 0 ||j == height - 1) {
-				printf("#");
+			if (i == 0 || i == width - 1) {
+				printf(w);
+			}
+			else if (j == 0 || j == height - 1) {
+				printf(h);
 			}
 			else {
 				printf(" ");
@@ -24,20 +29,15 @@ void draw() {
 void setup() {
 
 
-
-
-
-
-
 }
 int main() {
-	
+
 	cout << "To start the game type 1 followed by ENTER." << endl;
 	cin >> userInput;
 	if (userInput == 1) {
 		draw();
-		}
-			
+	}
+
 
 	return 0;
 }
