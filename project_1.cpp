ad26582a0972694c48728f62291c291b547ce139
OBN010 <115172405+OBN010@users.noreply.github.com>
2022-11-15 12:24:17 +0100

    Add files via upload

---
+++ project_1.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+int height = 20, width = 20, i, j, gameOver, score, userInput;
+
+
+//Function to draw a boundary
+void draw() {
+	for (i = 0; i < height; ++i) {
+		for (j = 0; j < width; j++) {
+			if (i == 0 || i == width - 1 || j == 0 ||j == height - 1) {
+				printf("#");
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
+void setup() {
+
+
+
+
+
+
+
+}
+int main() {
+	
+	cout << "To start the game type 1 followed by ENTER." << endl;
+	cin >> userInput;
+	if (userInput == 1) {
+		draw();
+		}
+			
+
+	return 0;
+}
